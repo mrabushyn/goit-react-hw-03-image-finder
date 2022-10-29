aadce2085331ba625b6758dfcb1fcbc1155cb237
mrabushyn <99904851+mrabushyn@users.noreply.github.com>
2022-10-29 13:41:14 +0300

    create src

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,15 @@
-export const App = () => {
-  return (
-    <div
+import React, {Component} from "react";
+import axios from "axios";
+import Select from "react-select";
+
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+axios.defaults.API_KEY = 'https://pixabay.com/api/';
+
+
+export class App extends Component {
+  render() {
+    return (
+      <div
       style={{
         height: '100vh',
         display: 'flex',
@@ -9,8 +18,9 @@ export const App = () => {
         fontSize: 40,
         color: '#010101'
       }}
-    >
-      React homework template
-    </div>
-  );
-};
+      >
+        React homework template
+      </div>
+    );
+  }
+}
