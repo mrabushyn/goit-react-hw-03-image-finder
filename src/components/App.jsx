2c8ed6193a317ab042ac554cffd05cb6bbe86637
mrabushyn <99904851+mrabushyn@users.noreply.github.com>
2022-10-30 14:38:19 +0200

    add serchbar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,46 +1,45 @@
-import React, {Component} from "react";
-import axios from "axios";
+import React, { Component } from 'react';
+// import axios from "axios";
 // import css from './styles.module.css'
-import { SearchBar } from "./Searchbar/Searchbar";
-import { ImageGallery } from "./ImageGallery/ImageGallery";
+import { SearchBar } from './Searchbar/Searchbar';
+import { ImageGallery } from './ImageGallery/ImageGallery';
 import { ImageGalleryItem } from './ImageGalleryItem/ImageGalleryItem';
 
-axios.defaults.baseURL = 'https://pixabay.com/api/';
-const API_KEY = '30026905-3ea175f3d607ccbd72fb69757'
+// axios.defaults.baseURL = 'https://pixabay.com/api/';
+// const API_KEY = '30026905-3ea175f3d607ccbd72fb69757'
 export class App extends Component {
   state = {
-    searchText: null,
+    searchText: '',
     page: 1,
     images: null,
     loading: false,
   };
 
-  async componentDidMount(prevProps, prevState) {
+  async componentDidUpdate(prevProps, prevState) {
     try {
-      this.setState({loading:true})
+      if (prevState.searchText !== this.state.searchText)
+        this.setState({ loading: true });
       const response = await axios.get(
         `?q=${this.state.searchText}&page=${this.state.page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
       );
       const images = response.data.hits;
 
       if (!this.state.images) {
-        this.setState({ images, loading:false });
+        this.setState({ images, loading: false });
       }
     } catch (error) {}
   }
 
-  handleSubmit = evt => {
-    evt.preventDefault();
-    let searchText = evt.target.elements.text.value;
-    searchText ? this.setState({ searchText }) : alert('ENTER TEXT');
-    evt.target.elements.text.value = '';
+  handleSearchSubmit = searchText => {
+    console.log(searchText);
+    this.setState({ searchText });
   };
 
   render() {
     return (
-      <div >
-        <SearchBar onSubmit={this.handleSubmit} />
-        {this.state.loading && <div >Loading......................</div>}
+      <div>
+        <SearchBar onSubmit={this.handleSearchSubmit} />
+        {this.state.loading && <div> Loading...................... </div>}
         {this.state.images && (
           <ImageGallery>
             <ImageGalleryItem value={this.state.images} />
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,26 +1,52 @@
 import React, {Component} from 'react';
 import css from './Searchbar.module.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 // import { AiOutlineSearch } from 'react-icons/bs';
 import { BsSearch } from 'react-icons/bs';
-export const SearchBar = ({onSubmit}) => {
 
-  return (
-    <header className={css.searchbar}>
-      <form className={css.form} onSubmit={onSubmit}>
-        <button type="submit" className={css.button}>
-          <BsSearch/>
-          <span className={css.buttonLabel}></span>
-        </button>
 
-        <input
-          className={css.input}
-          type="text"
-          name="text"
-          autoComplete="off"
-          autoFocus
-          placeholder="Search images and photos"
-        />
-      </form>
-    </header>
-  );
+export class SearchBar extends Component {
+  state = {
+    searchText: '',
+  }
+
+  handleChange = evt => {
+    this.setState({ searchText: evt.currentTarget.value.toLowerCase() });
+  }
+
+  handleSubmit = evt => {
+    evt.preventDefault();
+    if(this.state.searchText.trim() === '') {
+      Notify.failure('ENTER TEXT FOR SEARCH');
+      return;
+    }
+    this.props.onSubmit(this.state.searchText);
+    this.setState({searchText:''})
+    // let searchText = evt.target.elements.text.value;
+    // searchText ? this.setState({ searchText }) : alert('ENTER TEXT');
+    evt.target.elements.text.value = '';
+  };
+
+  render() {
+    return (
+      <header className={css.searchbar}>
+        <form className={css.form} onSubmit={this.handleSubmit}>
+          <button type="submit" className={css.button}>
+            <BsSearch />
+            <span className={css.buttonLabel}></span>
+          </button>
+
+          <input
+            className={css.input}
+            type="text"
+            name="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+            onChange={this.handleChange}
+          />
+        </form>
+      </header>
+    );
+  }
 };
