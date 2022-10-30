c36d8702fbb915767a2ec37aa6558062bb59da78
mrabushyn <99904851+mrabushyn@users.noreply.github.com>
2022-10-30 15:23:44 +0200

    add axios search

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-// import axios from "axios";
+import axios from "axios";
 // import css from './styles.module.css'
 import { SearchBar } from './Searchbar/Searchbar';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { ImageGalleryItem } from './ImageGalleryItem/ImageGalleryItem';
 
-// axios.defaults.baseURL = 'https://pixabay.com/api/';
-// const API_KEY = '30026905-3ea175f3d607ccbd72fb69757'
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+const API_KEY = '30026905-3ea175f3d607ccbd72fb69757'
 export class App extends Component {
   state = {
     searchText: '',
@@ -18,13 +18,11 @@ export class App extends Component {
   async componentDidUpdate(prevProps, prevState) {
     try {
       if (prevState.searchText !== this.state.searchText)
-        this.setState({ loading: true });
+        {this.setState({ loading: true });
       const response = await axios.get(
         `?q=${this.state.searchText}&page=${this.state.page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
       );
       const images = response.data.hits;
-
-      if (!this.state.images) {
         this.setState({ images, loading: false });
       }
     } catch (error) {}
