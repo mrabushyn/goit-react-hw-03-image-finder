f4ce1439f5dd067458d2b11c0a6bb4f24c779c45
mrabushyn <99904851+mrabushyn@users.noreply.github.com>
2022-11-02 15:30:37 +0200

    fix button

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,7 +9,6 @@ import { Loader } from './Loader/Loader';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { Button } from './Button/Button';
 
-
 axios.defaults.baseURL = 'https://pixabay.com/api/';
 const API_KEY = '30026905-3ea175f3d607ccbd72fb69757';
 
@@ -19,30 +18,46 @@ export class App extends Component {
     page: 1,
     images: null,
     loading: false,
+    perPage: 12,
   };
 
   async componentDidUpdate(_, prevState) {
     try {
-      const { page, searchText } = this.state;
-      if (prevState.searchText !== searchText || prevState.page !== page) {
+      const { page, searchText, perPage } = this.state;
+
+      if (prevState.searchText !== searchText) {
+        this.setState({ loading: true });
+        const response = await axios.get(
+          `?q=${searchText}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=${perPage}`
+        );
+        const images = response.data.hits;
+
+        if (images.length === 0) {
+          this.setState({ loading: false, images: null });
+          Notify.failure('Нічого не знайдено. Спробуйте ввести щось інше)))');
+        }
+
+        else {
+          this.setState({ images, loading: false });
+        }
+      }
+
+      if (prevState.page < page) {
         this.setState({ loading: true });
         const response = await axios.get(
           `?q=${searchText}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
         );
+        const resImg = response.data.hits;
+        let images = [...this.state.images, ...resImg];
 
-        const images = response.data.hits;
-
-        if (response.data.total === 0) {
-          this.setState({ loading: false, images: null });
-          Notify.failure('Нічого не знайдено. Спробуйте ввести щось інше)))');
-        } else this.setState({ images, loading: false });
+        this.setState({ images, loading: false });
       }
     } catch (error) {
       this.setState({ loading: false });
       Notify.failure('Ups... Щось пішло не так');
     }
   }
-  
+
   handleSearchSubmit = searchText => {
     this.setState({ searchText, page: 1 });
   };
@@ -52,7 +67,7 @@ export class App extends Component {
   };
 
   render() {
-    const { loading, images, searchText } = this.state;
+    let { loading, images, searchText, perPage } = this.state;
     return (
       <div>
         <SearchBar onSubmit={this.handleSearchSubmit} />
@@ -62,7 +77,9 @@ export class App extends Component {
             <ImageGalleryItem value={images} imgText={searchText} />
           </ImageGallery>
         )}
-        {images && <Button onButton={this.buttonLoadMore} />}
+        {images && images.length >= perPage && (
+          <Button onButton={this.buttonLoadMore} />
+        )}
       </div>
     );
   }
